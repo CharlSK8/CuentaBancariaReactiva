a96590377efeb162c8056e816909f28e0109fe19
Charlinson <charlinson2013@gmail.com>
2025-01-17 09:24:49 -0500

    feat: Implementar cursor tailable para eventos de auditoría por cuenta

--- src/main/java/com/banco/operaciones_bancarias/repository/IEventoAuditoriaRepository.java
+++ src/main/java/com/banco/operaciones_bancarias/repository/IEventoAuditoriaRepository.java
@@ -1,6 +1,7 @@
 package com.banco.operaciones_bancarias.repository;
 
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
 import org.springframework.stereotype.Repository;
 
 import com.banco.operaciones_bancarias.model.EventoAuditoria;
@@ -9,4 +10,8 @@ import reactor.core.publisher.Flux;
 
 @Repository
 public interface IEventoAuditoriaRepository extends ReactiveMongoRepository<EventoAuditoria, String>{
+
+    @Tailable
+	public Flux<EventoAuditoria> findWithTailableCursorByCuentaId(int cuentaId);
+
 }
