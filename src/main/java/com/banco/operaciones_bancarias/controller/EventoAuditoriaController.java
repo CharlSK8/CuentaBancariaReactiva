cb5e5115959310b4f6ea3564ec8e171efdc9d072
Charlinson <charlinson2013@gmail.com>
2025-01-17 09:05:04 -0500

    feat: crea controller para manejar los eventos de auditoría

---
+++ src/main/java/com/banco/operaciones_bancarias/controller/EventoAuditoriaController.java
@@ -0,0 +1,24 @@
+package com.banco.operaciones_bancarias.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.banco.operaciones_bancarias.model.EventoAuditoria;
+import com.banco.operaciones_bancarias.service.IEventoAuditoriaService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auditoria")
+@CrossOrigin(origins = "http://localhost:4200")
+@Tag(name = "EventoAuditoria", description = "API para eventos de auditoría")
+public class EventoAuditoriaController {
+}
