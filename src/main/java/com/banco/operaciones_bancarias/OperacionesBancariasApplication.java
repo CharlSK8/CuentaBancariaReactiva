2e427a794b0e2522dd4b574cc7c3b3acbab2bc6c
Charlinson <charlinson2013@gmail.com>
2025-01-17 08:59:51 -0500

    feat: crea el main de la aplicación

---
+++ src/main/java/com/banco/operaciones_bancarias/OperacionesBancariasApplication.java
@@ -0,0 +1,14 @@
+package com.banco.operaciones_bancarias;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class OperacionesBancariasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OperacionesBancariasApplication.class, args);
+	}
+
+}
