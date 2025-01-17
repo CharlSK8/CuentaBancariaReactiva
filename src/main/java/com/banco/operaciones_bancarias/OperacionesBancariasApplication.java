57d80a6e6e8b7f5f2a712fd84f11fe5662967f0c
Charlinson <charlinson2013@gmail.com>
2025-01-17 09:00:16 -0500

    chore: agrega configuración para Swagger

--- src/main/java/com/banco/operaciones_bancarias/OperacionesBancariasApplication.java
+++ src/main/java/com/banco/operaciones_bancarias/OperacionesBancariasApplication.java
@@ -3,8 +3,11 @@ package com.banco.operaciones_bancarias;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Operaciones Bancarias API", version = "1.0.0", description = "API para operaciones bancarias"))
 public class OperacionesBancariasApplication {
 
 	public static void main(String[] args) {
