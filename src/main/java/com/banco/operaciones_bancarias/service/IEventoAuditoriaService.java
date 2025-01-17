cc21cb71cef80c83efe44b7cf85bbbbd093267cf
Charlinson <charlinson2013@gmail.com>
2025-01-17 09:43:50 -0500

    feat: crea service e implementación para EventoAuditoria

---
+++ src/main/java/com/banco/operaciones_bancarias/service/IEventoAuditoriaService.java
@@ -0,0 +1,8 @@
+package com.banco.operaciones_bancarias.service;
+
+import com.banco.operaciones_bancarias.model.EventoAuditoria;
+
+import reactor.core.publisher.Flux;
+
+public interface IEventoAuditoriaService {
+}
