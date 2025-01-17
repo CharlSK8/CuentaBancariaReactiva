6d2d5fe1241171921ea97f1e26e25f527489dac2
Charlinson <charlinson2013@gmail.com>
2025-01-17 08:55:09 -0500

    feat: crea clase de constants para manejar valores estáticos y reutilizables

---
+++ src/main/java/com/banco/operaciones_bancarias/utils/Constants.java
@@ -0,0 +1,15 @@
+package com.banco.operaciones_bancarias.utils;
+
+public class Constants {
+
+    private Constants(){
+        throw new IllegalStateException("Utility class");
+    }
+
+    public static final String RETIRO = "RETIRO";
+    public static final String DEPOSITO = "DEPOSITO";
+    public static final String INICIO = "INICIO";
+    public static final String EXITO = "EXITO";
+    public static final String ERROR = "ERROR";
+
+}
