f90f83057c8418419cc93185b1a236e266af9f7b
Charlinson <charlinson2013@gmail.com>
2025-01-17 09:23:56 -0500

    feat: crea repository reactivo para manejar las consultas de EventoAuditoria

---
+++ src/main/java/com/banco/operaciones_bancarias/repository/IEventoAuditoriaRepository.java
@@ -0,0 +1,12 @@
+package com.banco.operaciones_bancarias.repository;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.banco.operaciones_bancarias.model.EventoAuditoria;
+
+import reactor.core.publisher.Flux;
+
+@Repository
+public interface IEventoAuditoriaRepository extends ReactiveMongoRepository<EventoAuditoria, String>{
+}
