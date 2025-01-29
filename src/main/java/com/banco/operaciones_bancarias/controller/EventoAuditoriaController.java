5251fdbde1f93a89f816c227aee61ccbad028c38
Charlinson <charlinson2013@gmail.com>
2025-01-29 17:27:56 -0500

    feat: añade manejo de errores en el controller

--- src/main/java/com/banco/operaciones_bancarias/controller/EventoAuditoriaController.java
+++ src/main/java/com/banco/operaciones_bancarias/controller/EventoAuditoriaController.java
@@ -1,11 +1,13 @@
 package com.banco.operaciones_bancarias.controller;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import com.banco.operaciones_bancarias.model.EventoAuditoria;
 import com.banco.operaciones_bancarias.service.IEventoAuditoriaService;
@@ -13,8 +15,10 @@ import com.banco.operaciones_bancarias.service.IEventoAuditoriaService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import reactor.core.publisher.Flux;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/auditoria")
@@ -27,7 +31,11 @@ public class EventoAuditoriaController {
     @GetMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
     @Operation(summary = "Stream de eventos de auditoría", description = "Stream de eventos de auditoría para una cuenta específica")
     public Flux<EventoAuditoria> streamEventoAuditoria(@RequestParam int cuentaId) {
-        return eventoAuditoriaService.streamEventosAuditoria(cuentaId);
+        return eventoAuditoriaService.streamEventosAuditoria(cuentaId)
+                .onErrorResume(e -> {
+                    log.error("Error en el stream de eventos de auditoría para cuentaId: {}", cuentaId, e);
+                    return Flux.error(new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "Error al recuperar eventos de auditoría"));
+                });
     }
 
 }
