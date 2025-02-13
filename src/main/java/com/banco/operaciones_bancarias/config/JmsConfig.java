e4df5872ccd7977494a10ea71f037b07be387065
daniel.rua@sofka.com.co <daniel.rua@sofka.local>
2025-02-13 12:13:44 -0500

    Se crea ActiveMQ de consumidor para auditoria

---
+++ src/main/java/com/banco/operaciones_bancarias/config/JmsConfig.java
@@ -0,0 +1,23 @@
+package com.banco.operaciones_bancarias.config;
+
+import jakarta.jms.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+
+@Configuration
+@EnableJms
+public class JmsConfig {
+
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory){
+        DefaultJmsListenerContainerFactory jmsListenerContainerFactory = new DefaultJmsListenerContainerFactory();
+
+        jmsListenerContainerFactory.setConnectionFactory(connectionFactory);
+        jmsListenerContainerFactory.setConcurrency("20-25");
+
+        return jmsListenerContainerFactory;
+    }
+
+}
