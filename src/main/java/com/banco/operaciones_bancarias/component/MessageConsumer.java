7d4fcf7f36a58b7b3ee27b7bdeb13d622f183820
julian.huerfano <julianest12@gmail.com>
2025-02-13 21:42:26 -0500

    FEAT Listeners ActivemQ

---
+++ src/main/java/com/banco/operaciones_bancarias/component/MessageConsumer.java
@@ -0,0 +1,45 @@
+package com.banco.operaciones_bancarias.component;
+
+import jakarta.jms.TextMessage;
+import org.apache.activemq.command.ActiveMQTextMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jms.annotation.JmsListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageConsumer {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MessageConsumer.class);
+
+    @JmsListener(destination = "auth-queue", containerFactory = "jmsListenerContainerFactory")
+    public void listenAuthQueue(Object eventMessage) {
+        processMessage(eventMessage, "auth-queue");
+    }
+
+    @JmsListener(destination = "op_bank_react-queue", containerFactory = "jmsListenerContainerFactory")
+    public void listenOpBankReactQueue(Object eventMessage) {
+        processMessage(eventMessage, "op_bank_react-queue");
+    }
+
+    public void processMessage(Object eventMessage, String queueName) {
+        try {
+            String jsonMessage;
+
+            // Extraer el contenido del mensaje si es de tipo ActiveMQTextMessage
+            if (eventMessage instanceof ActiveMQTextMessage) {
+                jsonMessage = ((ActiveMQTextMessage) eventMessage).getText();
+            } else if (eventMessage instanceof TextMessage) {
+                jsonMessage = ((TextMessage) eventMessage).getText();
+            } else {
+                LOGGER.warn("Tipo de mensaje desconocido: {}", eventMessage.getClass().getName());
+                return;
+            }
+            System.out.println("📩 Evento recibido de " + queueName + ": " + jsonMessage);
+            LOGGER.info("JSON Message received from {}: {}", queueName, jsonMessage);
+
+        } catch (Exception e) {
+            LOGGER.error("Error processing message from {}: {}", queueName, e.getMessage(), e);
+        }
+    }
+}
--- src/main/java/com/banco/operaciones_bancarias/config/JmsConfig.java
+++ src/main/java/com/banco/operaciones_bancarias/config/JmsConfig.java
@@ -5,19 +5,38 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jms.annotation.EnableJms;
 import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.support.converter.MappingJackson2MessageConverter;
+import org.springframework.jms.support.converter.MessageConverter;
+import org.springframework.jms.support.converter.MessageType;
 
 @Configuration
 @EnableJms
 public class JmsConfig {
 
     @Bean
-    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory){
-        DefaultJmsListenerContainerFactory jmsListenerContainerFactory = new DefaultJmsListenerContainerFactory();
+    public MessageConverter jacksonJmsMessageConverter() {
+        MappingJackson2MessageConverter converter = new MappingJackson2MessageConverter();
+        converter.setTargetType(MessageType.TEXT); // Enviar como JSON String
+        converter.setTypeIdPropertyName("_type"); // Necesario para la deserialización automática
+        return converter;
+    }
 
-        jmsListenerContainerFactory.setConnectionFactory(connectionFactory);
-        jmsListenerContainerFactory.setConcurrency("20-25");
+    @Bean
+    public DefaultJmsListenerContainerFactory jmsListenerContainerFactory(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setConcurrency("5-10");
+        factory.setMessageConverter(messageConverter); // Configuramos el conversor aquí
+        return factory;
+    }
 
-        return jmsListenerContainerFactory;
+    @Bean
+    public JmsTemplate jmsTemplate(ConnectionFactory connectionFactory, MessageConverter messageConverter) {
+        JmsTemplate jmsTemplate = new JmsTemplate(connectionFactory);
+        jmsTemplate.setMessageConverter(messageConverter); // Configuramos el conversor aquí
+        return jmsTemplate;
     }
 
 }
+
